336d2476b371655d53a26b58486843490f0e811d
ritikattri-KING <ritik.attri2024@nst.rishihood.edu.in>
2025-12-04 12:26:39 +0530

    Update index.js

--- Easy 1/index.js
+++ Easy 1/index.js
@@ -1,5 +1,12 @@
-const fs = require("fs");
+const fs = require('fs');
+const path = require('path');
 
 function writeDataFile() {
+  // Construct the absolute path to file.txt
+  const filePath = path.join(__dirname, 'file.txt');
 
+  // Write the data synchronously
+  fs.writeFileSync(filePath, 'We will not miss the exam next time');
 }
+
+module.exports = writeDataFile;
